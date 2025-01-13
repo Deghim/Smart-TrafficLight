d46d8c9951c5c996684a25255b2152907bc57a89
Jorge Chavira <jorgechavira4@gmail.com>
2025-01-13 10:06:22 -0600

    Se agregó deteccion de personas

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ license_plate_detector  = YOLO('license_plate_detector.pt' ) # Se crea un modelo
 
 """ Load Videos """
 # cap = cv2.VideoCapture('testvideos/27260-362770008_tiny.mp4') # Se utiliza un video para testear el modelo
-stream = cv2.VideoCapture('testvideos/CaravanCouple.mp4') # Se utiliza un video para testear el modelo
+# stream = cv2.VideoCapture('testvideos/PeopleWalking.mp4') # Se utiliza un video para testear el modelo
 
-# stream = cv2.VideoCapture(0)
+stream = cv2.VideoCapture(0)
 
 """ Model Variables """
 vehicles = [2,3,5,6,7,8] # Aqui se almacenan las id's de las clases pertenecientes de la clase vehiculos del dataset de coco
-civilians = [1]
+civilians = [0]
 results = {}
 
 def draw_detection_boxes(frame, detection, color = (255,255,255)):
@@ -24,17 +24,25 @@ def draw_detection_boxes(frame, detection, color = (255,255,255)):
         for box in result.boxes:
             cls_id = int(box.cls[0].item()) # Get class ID and check if it's a vehicle
 
+            # print(f"{cls_id}: {result.names[cls_id]}")
+
             if cls_id in vehicles:
                 x1, y1, x2, y2 = map(int, box.xyxy[0].cpu().numpy()) # Extract coordinates and convert to integers
                 confidence = float(box.conf[0].cpu().numpy())
                 
                 cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)# Draw rectangle around vehicle
                 
-                label = f"{result.names[cls_id]}: {confidence:.2f}" # Add label with class name and confidence
+                label = f"id:{cls_id}, {result.names[cls_id]}: {confidence:.2f}" # Add label with class name and confidence
+                cv2.putText(frame, label, (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    
+            if cls_id in civilians:
+                x1, y1, x2, y2 = map(int, box.xyxy[0].cpu().numpy()) # Extract coordinates and convert to integers
+                confidence = float(box.conf[0].cpu().numpy())
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0,0,255), 2)# Draw rectangle around vehicle
                 
-                cv2.putText(frame, label, (x1, y1-10),
-                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-                
+                label = f"id:{cls_id}, {result.names[cls_id]}: {confidence:.2f}" # Add label with class name and confidence
+                cv2.putText(frame, label, (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
     return frame
 
 def video_stream():
