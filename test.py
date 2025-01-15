34763f11001c233f9b869bca321b86bd07821350
Jorge Chavira <jorgechavira4@gmail.com>
2025-01-15 07:16:47 -0600

    Se uso el metodo track con el tracker de bytes

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 from ultralytics import YOLO # type: ignore
 
-# model = YOLO('yolov8n.pt')
+model = YOLO('yolov8n.pt')
 # licenses = YOLO('license_plate_detector.pt')
 
-# results = model("testvideos/CaravanCouple.mp4",show=True,save=False)
+# results = model("testvideos/CaravanCouple.mp4",show=True,save=False) 
+
+# results = model.track("testvideos/Wondercamp.mp4",show=True,save=False, tracker='bytetrack.yaml')
+results = model.track("testvideos/Wondercamp.mp4",show=True,save=False, tracker='bytetrack.yaml')
 
 # juan = licenses("testvideos/CaravanCouple.mp4",show=True,save=True)
 
